ca819b68a13780fcf49839eca0be98393c7972ae
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-13 09:59:28 +0530

    question 5

--- day_3_problems/src/Question_5.java
+++ day_3_problems/src/Question_5.java
@@ -1,44 +1,46 @@
-//Question 5
-//CP - Write a program SpringSeason that takes two int values month and day from the command line and
-//prints “Its a Spring Season” otherwise prints “Not a Spring Season”.
-//Hint =>
-//        1. Spring Season is from March 20 to June 2
-
 import java.util.Scanner;
 
 public class Question_5 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter month : ");
+        System.out.print("Enter month: ");
         int month = sc.nextInt();
 
-        System.out.print("Enter day : ");
+        System.out.print("Enter day: ");
         int day = sc.nextInt();
+
+        boolean isSpring = false;
 
         switch (month) {
             case 3:
                 if (day >= 20 && day <= 31) {
-                    System.out.println("Its a Spring Season");
-                    return;
+                    isSpring = true;
                 }
-                case 4:
+                break;
+            case 4:
                 if (day >= 1 && day <= 30) {
-                    System.out.println("Its a Spring Season");
-                    return;
+                    isSpring = true;
                 }
-                case 5:
+                break;
+            case 5:
                 if (day >= 1 && day <= 31) {
-                    System.out.println("Its a Spring Season");
-                    return;
+                    isSpring = true;
                 }
-                case 6:
+                break;
+            case 6:
                 if (day >= 1 && day <= 2) {
-                    System.out.println("Its a Spring Season");
-                    return;
+                    isSpring = true;
                 }
+                break;
         }
-        System.out.println("Not a Spring Season");
+
+        if (isSpring) {
+            System.out.println("It's a Spring Season");
+        } else {
+            System.out.println("Not a Spring Season");
+        }
+
 
     }
 }
