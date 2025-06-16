2acfd72380834a0b24f2ad2ff3f0b71f1187da98
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-16 08:56:52 +0530

    [kartikay] Add question 10

--- control_flows_/src/Question_10.java
+++ control_flows_/src/Question_10.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
+//Question 10
+//CP - Write a program to check if a number is divisible by 5
+//I/P => number
+//O/P => Is the number ___ divisible by 5? ___
 public class Question_10 {
     public static void main(String[] args) {
-        System.out.println("This is Question_10");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n%5 == 0){
+            System.out.println("Number divisible by 5");
+        }else{
+            System.out.println("Number is not divisible by 5");
+        }
     }
 }
