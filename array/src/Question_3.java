3569198a976941721759b457bc7c7661658f3d4d
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-16 10:47:54 +0530

    [kartikay] Add question 3 array

--- array/src/Question_3.java
+++ array/src/Question_3.java
@@ -1,15 +1,30 @@
+//Question 3
+//CP - Create a program to print a multiplication table of a number.
+//        Hint =>
+//        1. Get an integer input and store it in the number variable. Also, define an integer array to store the results
+//of multiplication from 1 to 10
+//        2. Run a loop from 1 to 10 and store the results in the multiplication table array
+//3. Finally, display the result from the array in the format number * i = ___
+
+
 import java.util.Scanner;
 
-//Question 3
-//CP - Write a Program to find the factorial of an integer entered by the user.
-//Hint =>
-//        1. For example, the factorial of 4 is 1 * 2 * 3 * 4 which is 24.
-//        2. Take an integer input from the user and assign it to the variable. Check the user has entered a positive
-//integer.
-// 3. Using a while loop, compute the factorial.
-// 4. Print the factorial at the end
 public class Question_3 {
     public static void main(String[] args) {
-        System.out.println(1.53 % 1.0);
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+
+        int[] table = new int[10];
+
+        for (int i = 1; i <= 10 ; i++) {
+            table[i-1] = number*i;
+        }
+
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(number+" x "+i+1+" = "+table[i]);
+        }
+
+
     }
 }
