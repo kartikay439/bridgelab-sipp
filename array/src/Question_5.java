2597340530c1368793cc983c7f2a0a86925eb762
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-16 10:57:18 +0530

    [kartikay] Add question 5 array

--- array/src/Question_5.java
+++ array/src/Question_5.java
@@ -1,46 +1,32 @@
+//Question 5
+//CP - Create a program to find the multiplication table of a number entered by the user from 6 to 9 and display
+//the result
+//Hint =>
+//        1. Take integer input and store it in the variable number, as well as define an integer array to store the
+//multiplication result in the variable multiplicationResult
+//2. Using a for loop, find the multiplication table of numbers from 6 to 9 and save the result in the array
+//3. Finally, display the result from the array in the format number * i = ___
+
 import java.util.Scanner;
 
 public class Question_5 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter month: ");
-        int month = sc.nextInt();
+        System.out.print("Enter a number to get its multiplication table from 6 to 9: ");
+        int number = sc.nextInt();
 
-        System.out.print("Enter day: ");
-        int day = sc.nextInt();
+        int[] multiplicationResult = new int[4]; // For multipliers 6, 7, 8, 9
 
-        boolean isSpring = false;
-
-        switch (month) {
-            case 3:
-                if (day >= 20 && day <= 31) {
-                    isSpring = true;
-                }
-                break;
-            case 4:
-                if (day >= 1 && day <= 30) {
-                    isSpring = true;
-                }
-                break;
-            case 5:
-                if (day >= 1 && day <= 31) {
-                    isSpring = true;
-                }
-                break;
-            case 6:
-                if (day >= 1 && day <= 2) {
-                    isSpring = true;
-                }
-                break;
+        int index = 0;
+        for (int i = 6; i <= 9; i++) {
+            multiplicationResult[index] = number * i;
+            index++;
         }
 
-        if (isSpring) {
-            System.out.println("It's a Spring Season");
-        } else {
-            System.out.println("Not a Spring Season");
+        System.out.println("\nMultiplication Table:");
+        for (int i = 0; i < multiplicationResult.length; i++) {
+            System.out.println(number + " * " + (i + 6) + " = " + multiplicationResult[i]);
         }
-
-
     }
 }
