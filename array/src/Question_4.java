acc06c53ba826931289eda131e891ce14ae25e43
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-16 10:52:44 +0530

    [kartikay] Add question 3 array

--- array/src/Question_4.java
+++ array/src/Question_4.java
@@ -1,23 +1,18 @@
 import java.util.Scanner;
 
 //Question 4
-//CP - Write a Program to find the factorial of an integer entered by the user.
+//CP - Write a program to take user input for the age of all 10 students in a class and check whether the
+//        student can vote depending on his/her age is greater or equal to 18.
 //Hint =>
-//        1. For example, the factorial of 4 is 1 * 2 * 3 * 4 which is 24.
-//        2. Take an integer input from the user and assign it to the variable. Check the user has entered a positive
-//integer.
-// 3. Using a for loop, compute the factorial.
-// 4. Print the factorial at the end
+//        1. Define an array of 10 integer elements and take user input for the student's age.
+//        2. Loop through the array using the length property and for the element of the array check If the age is a
+//negative number print an invalid age and if 18 or above, print The student with the age ___ can vote.
+//Otherwise, print The student with the age ___ cannot vote.
+
+
 public class Question_4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number : ");
-        int number = sc.nextInt();
-        int factorial = 1;
-        for (int i = 2; i <=number ; i++) {
-            factorial *= i;
-        }
 
-        System.out.println("Factorial : "+factorial);
+
     }
 }
