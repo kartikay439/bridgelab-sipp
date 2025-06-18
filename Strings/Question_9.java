15f29658dbd321e7b2913da450a3987d4c88a9e9
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-18 22:19:05 +0530

    [kartikay] Add question 8 string

---
+++ Strings/Question_9.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Question_9 {
+    public static boolean compareStrings(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String s1 = scanner.next();
+        System.out.print("Enter second string: ");
+        String s2 = scanner.next();
+
+        boolean customEqual = compareStrings(s1, s2);
+        boolean builtInEqual = s1.equals(s2);
+
+        System.out.println("Custom comparison: " + customEqual);
+        System.out.println("Built-in comparison: " + builtInEqual);
+        System.out.println("Results match: " + (customEqual == builtInEqual));
+
+        scanner.close();
+    }
+}
