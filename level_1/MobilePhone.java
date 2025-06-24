17f205d6d56507a84d905d3c942cd83b4b02849b
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-24 11:52:31 +0530

    [kartikay] added MobilePhone.java

---
+++ level_1/MobilePhone.java
@@ -0,0 +1,20 @@
+package level_1;
+
+// Program to Handle Mobile Phone Details
+// Problem Statement: Create a MobilePhone class with attributes brand, model, and
+// price. Add a method to display all the details of the phone. The MobilePhone class uses
+// attributes to store the phone&#39;s characteristics. The method is used to retrieve and
+// display this information for each object.
+
+public class MobilePhone {
+    String brand ; 
+    String model;
+    Double price;
+
+      void ShowItemDetail(){
+        System.out.println("Brand : "+brand);
+        System.out.println("Item Code: "+itemCode);
+        System.out.println("Price : "+price);
+     }
+    
+}
