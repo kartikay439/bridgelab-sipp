7ef00b68b9a4c628b9c13940ab9b4135c2d53ffb
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-25 09:47:02 +0530

    [kartikay] added Employee.java

---
+++ constructor/Employee.java
@@ -0,0 +1,39 @@
+package constructor;
+
+class Employee {
+    public int employeeID;
+    protected String department;
+    private double salary;
+
+    public Employee(int employeeID, String department, double salary) {
+        this.employeeID = employeeID;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary >= 0) {
+            this.salary = salary;
+        }
+    }
+}
+
+class Manager extends Employee {
+    private int teamSize;
+
+    public Manager(int employeeID, String department, double salary, int teamSize) {
+        super(employeeID, department, salary);
+        this.teamSize = teamSize;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee ID : " + employeeID);
+        System.out.println("Department  : " + department);
+        System.out.println("Salary      : ₹" + getSalary());
+        System.out.println("Team Size   : " + teamSize);
+    }
+}
