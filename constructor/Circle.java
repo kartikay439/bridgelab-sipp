e4fab8e9ad68621edae281c6cadc404bae4a60f2
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-25 08:38:25 +0530

    [kartikay] added Circle.java

---
+++ constructor/Circle.java
@@ -0,0 +1,13 @@
+package constructor;
+
+public class Circle {
+    double radius;
+
+    public Circle() {
+    this.radius = 0d;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    } 
+}
