c678699f2d5cdfd0ca74385b9b40704d0186870e
kartikay439 <kartikaytrivedi99@gmail.com>
2025-06-25 10:07:01 +0530

    [kartikay] added BankAccount.java

---
+++ static_this_final_instanceOf/BankAccount.java
@@ -0,0 +1,36 @@
+package static_this_final_instanceOf;
+
+public class BankAccount {
+    //This will remain sane across all objects
+    static String bankName;
+
+    //accountCount will store accounts quantity
+    static int accountCounts = 0;
+
+    String accountHolderName;
+    final int accountNumber;
+
+    public BankAccount(String accountHolderName, int accountNumber) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        accountCounts++;
+    }
+
+    static int getTotalAccounts(){
+        return accountCounts;
+    }
+    
+    public void acchountDetail() {
+
+        if(this instanceof BankAccount){
+        System.out.println("Account Number : " + accountNumber);
+        System.out.println("Account Holder : " + accountHolderName);
+        }
+        
+    }
+
+
+
+    
+
+}
