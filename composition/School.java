0153ae199d38b69c69bb8da079ba5c801001fb8b
kartikay439 <kartikaytrivedi99@gmail.com>
2025-07-01 18:07:35 +0530

    [kartikay] workshop question School.java

--- composition/School.java
+++ composition/School.java
@@ -1,78 +1,51 @@
-package composition;
-
-import java.util.*;
-
-// Course class
 class Course {
-    private String courseName;
-    private Set<Student> enrolledStudents;
+    String name;
+    ArrayList<Student> students = new ArrayList<>();
 
-    public Course(String courseName) {
-        this.courseName = courseName;
-        this.enrolledStudents = new HashSet<>();
+    public Course(String name) {
+        this.name = name;
     }
 
-    public String getCourseName() {
-        return courseName;
+    public void enroll(Student s) {
+        students.add(s);
+        s.enroll(this);
     }
 
-    public void enrollStudent(Student student) {
-        enrolledStudents.add(student);
-    }
-
-    public void showEnrolledStudents() {
-        System.out.println("Students enrolled in " + courseName + ":");
-        for (Student student : enrolledStudents) {
-            System.out.println("- " + student.getName());
-        }
+    public void showStudents() {
+        System.out.println("Course: " + name);
+        for (Student s : students)
+            System.out.println("- " + s.name);
     }
 }
 
-// Student class
 class Student {
-    private String name;
-    private Set<Course> enrolledCourses;
+    String name;
+    ArrayList<Course> courses = new ArrayList<>();
 
     public Student(String name) {
         this.name = name;
-        this.enrolledCourses = new HashSet<>();
     }
 
-    public String getName() {
-        return name;
+    public void enroll(Course c) {
+        if (!courses.contains(c)) courses.add(c);
     }
 
-    public void enrollInCourse(Course course) {
-        enrolledCourses.add(course);
-        course.enrollStudent(this);
-    }
-
-    public void showEnrolledCourses() {
-        System.out.println(name + " is enrolled in:");
-        for (Course course : enrolledCourses) {
-            System.out.println("- " + course.getCourseName());
-        }
+    public void showCourses() {
+        System.out.println("Student: " + name);
+        for (Course c : courses)
+            System.out.println("- " + c.name);
     }
 }
 
-// School class (Aggregation)
 class School {
-    private String name;
-    private List<Student> students;
+    String name;
+    ArrayList<Student> students = new ArrayList<>();
 
     public School(String name) {
         this.name = name;
-        this.students = new ArrayList<>();
     }
 
-    public void addStudent(Student student) {
-        students.add(student);
-    }
-
-    public void showAllStudents() {
-        System.out.println("Students in " + name + " School:");
-        for (Student student : students) {
-            System.out.println("- " + student.getName());
-        }
+    public void addStudent(Student s) {
+        students.add(s);
     }
 }
