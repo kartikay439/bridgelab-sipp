ee5af92936b29d3f550b31bf84867070e59a5132
kartikay439 <kartikaytrivedi99@gmail.com>
2025-07-02 04:50:19 +0530

    [kartikay] workshop question Vehicle.java

---
+++ inheritance/Vehicle.java
@@ -0,0 +1,57 @@
+package inheritance;
+
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    public Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("Speed: " + maxSpeed + " km/h, Fuel: " + fuelType);
+    }
+}
+
+class Car extends Vehicle {
+    int seatCapacity;
+
+    public Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+class Truck extends Vehicle {
+    int loadCapacity;
+
+    public Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " kg");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    boolean hasCarrier;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasCarrier) {
+        super(maxSpeed, fuelType);
+        this.hasCarrier = hasCarrier;
+    }
+
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Carrier: " + hasCarrier);
+    }
+}
